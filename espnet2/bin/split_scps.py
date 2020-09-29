e8d4da33552513f7472e5c74e591d88041e37b76
WuHao9408 <40881578+WuHao9408@users.noreply.github.com>
2020-09-29 18:06:58 +0800

    Update split_scps.py
    when i use 52GB data to train the LM, the split_scp.py estimated processing time exceeds 20 days. Because each line of text writting requires once open file operation and 52GB text, over 400million lines in total. 
    I try to construct two temporary lists which used to store lines until the number over "templine_num", then write two list in text and clear them. If this variable is larger, the processing is faster. Be care of oom.

--- espnet2/bin/split_scps.py
+++ espnet2/bin/split_scps.py
@@ -34,6 +34,74 @@ def split_scps(
         (Path(output_dir) / name).mkdir(parents=True, exist_ok=True)
 
     scp_files = [open(s, "r", encoding="utf-8") for s in scps]
+    
+    
+    
+    # ===================== replace ============================
+    for n in range(num_splits):
+        for name in names:
+            if (Path(output_dir) / name / f"split.{n}").exists():
+                (Path(output_dir) / name / f"split.{n}").unlink()
+                
+    temp_line1=[]
+    temp_line2=[]
+    for i in range(num_splits):
+        temp_line1.append([])
+        temp_line2.append([])
+                
+    counter = Counter()
+    linenum = -1
+             
+    for linenum, lines in enumerate(zip_longest(*scp_files)):
+        if any(line is None for line in lines):
+            raise RuntimeError("Number of lines are mismatched")
+
+        prev_key = None
+        for line in lines:
+            key = line.rstrip().split(maxsplit=1)[0]
+            if prev_key is not None and prev_key != key:
+                raise RuntimeError("Not sorted or not having same keys")
+
+        # Select a piece from split texts alternatively
+        num = linenum % num_splits
+        counter[num] += 1
+        
+        templine_num = 10000  #500000   #this is the maximum number of lines in temp_line, but be care of the problem (out of memory).
+        
+        for line, name in zip(lines, names):
+            if name == names[0]:
+                temp_line1[num].append(line)
+            else:
+                temp_line2[num].append(line)
+            if len(temp_line1[num]) == len(temp_line2[num]) and len(temp_line2[-1])>=templine_num:
+                for split_id in range(len(temp_line1)):
+                    with (Path(output_dir)/names[0]/f"split.{split_id}").open("a", encoding="utf-8") as f:
+                        for j in temp_line1[split_id]:
+                            f.write(j)
+                for split_id in range(len(temp_line2)):
+                    with (Path(output_dir)/names[1]/f"split.{split_id}").open("a", encoding="utf-8") as f:
+                        for j in temp_line2[split_id]:
+                            f.write(j)
+                for i in range(len(temp_line1)):
+                    temp_line1[i]=[]
+                for i in range(len(temp_line2)):
+                    temp_line2[i]=[]
+                    
+    if temp_line1!=[]:
+        for split_id in range(len(temp_line1)):
+            with (Path(output_dir)/names[0]/f"split.{split_id}").open("a", encoding="utf-8") as f:
+                for j in temp_line1[split_id]:
+                    f.write(j)
+        for split_id in range(len(temp_line2)):
+            with (Path(output_dir)/names[1]/f"split.{split_id}").open("a", encoding="utf-8") as f:
+                for j in temp_line2[split_id]:
+                    f.write(j)
+    
+    del temp_line1
+    del temp_line2
+    
+    '''
+    # ====================== original code ======================
     # Remove existing files
     for n in range(num_splits):
         for name in names:
@@ -62,7 +130,10 @@ def split_scps(
                 "a", encoding="utf-8"
             ) as f:
                 f.write(line)
-
+    '''
+    
+    
+    
     if linenum + 1 < num_splits:
         raise RuntimeError(
             f"The number of lines is less than num_splits: {linenum + 1} < {num_splits}"
